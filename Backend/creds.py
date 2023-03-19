6a7950f48e5c3b9517f08e2d1194cb82e75567c4
wakindo <70498088+wakindo@users.noreply.github.com>
2023-03-18 22:25:37 -0500

    Update creds.py

--- Backend/creds.py
+++ Backend/creds.py
@@ -1,11 +1,23 @@
-#
-# Use these creds for all connection to our AWS DEV DB
 #=======================================================
-# Change to Prod DB info. once Jacob & Zach are done.
+# Use these creds for all connection to our AWS DEV DB #
 #=======================================================
-
 class Creds:
     conString = 'ovvidbinstance.c9rwb1hk6i86.us-east-1.rds.amazonaws.com'
     userName = 'OvviDBA'
     password = 'OvviAdmin2023'
-    dbName = 'OvviDevDB'
+    dbName = 'OvviDevDB' 
+    
+
+
+#========================================================
+# Change to Prod DB info. once DEV TESTS are done       #
+# Just uncomment next block and comment previous one    #
+#========================================================
+
+""" 
+class Creds:
+    conString = 'rds-mysql-ovviproject.cqw9wvzdacxo.us-east-1.rds.amazonaws.com'
+    userName = 'admin'
+    password = 'Ovvi!123'
+    dbName = 'ovvi' 
+ """
