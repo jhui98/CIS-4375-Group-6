3435ef46effff7c28e8700a28c94b73d774aedd0
parthppatel7 <77832733+parthppatel7@users.noreply.github.com>
2023-03-27 20:11:57 -0500

    Hardware page updates

--- Frontend/src/views/Hardware.vue
+++ Frontend/src/views/Hardware.vue
@@ -1,6 +1,37 @@
+<script>
+export default {
+  data() {
+    return {
+      message: ''
+    }
+  }
+}
+</script>
+
+<style>
+.edit {
+  background-color: #4CAF50; /* Green */
+}
+.add {
+  background-color: #008CBA; /* Blue */
+}
+.delete {
+  background-color: #f44336; /* Red */
+}
+
+</style>
+
 <template>
     <main class="Hardware-page">
         <h1>Hardware</h1>
+        <br>
         <p>This is the Hardware page</p>
+        <br>
+        <p>Enter a Hardware ID: {{ message }}
+        <input v-model="message" placeholder="Hardware ID" /></p>
+        <br>
+        <button class="add" type='submit'>Add</button>
+        <button class="edit" type='submit'>Edit</button>
+        <button class="delete" type='submit'>Delete</button>
     </main>
 </template>
