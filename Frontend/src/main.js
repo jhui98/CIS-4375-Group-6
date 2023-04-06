3cdd60990311ad10a10b2c519de0b10b0e76e3da
JoshuaASoto <joshua.antonio.soto@gmail.com>
2023-04-06 00:40:55 -0500

    experiment

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-
-
 import router from "./router"
+// import BootstrapVue from 'bootstrap-vue';
+
+
+
+
 
 
 createApp(App).use(router).mount('#app')
--- Frontend/src/views/Iso.vue
+++ Frontend/src/views/Iso.vue
@@ -98,5 +98,47 @@ export default {
             </div>
           </form>
         </div>
+
+        <div class="jumbotron vertical center">
+            <div class="container">
+                
+                <div class="row">
+                    <div class="col-sm-12">
+                        <p> Header</p>
+
+                        <table class="table table-hover">
+                            <!-- Table Head-->
+                            <thead>
+                                <tr>
+                                    <!--Table Head cells-->
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Action</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="item, ISO_ID in isoData" :key="ISO_ID">
+                                    <td> {{item.ISO_ID}} </td>
+                                    <td> {{ item.ISO_COMPANY }} </td>
+                                    <td> 
+                                        <div class="btn-group" role="group">
+                                            <button type="button" class="btn btn-info
+                                            btn-sm">Update</button>
+                                            <button type="button" class="btn btn-danger
+                                            btn-sm" @click ="deleteISO(buttonid)">Delete</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
     </main>
 </template>
