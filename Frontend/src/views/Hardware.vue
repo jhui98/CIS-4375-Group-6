d6696775d642fcb3c94f44d82bee35fc883949ef
parthppatel7 <77832733+parthppatel7@users.noreply.github.com>
2023-04-08 17:34:40 -0500

    Updated scripting functions to Hardware page

--- Frontend/src/views/Hardware.vue
+++ Frontend/src/views/Hardware.vue
@@ -1,10 +1,59 @@
 <script>
+let apiURL = `http://localhost:5000/api/hardware`;
+import axios from "axios";
 export default {
   data() {
     return {
-      message: ''
+      hardwaretype: {
+        hardware_id: "",
+        hardware_name: "",
+        model_number: "",
+        htype_id: ""
+      },
+      hardwareData:[]
     }
-  }
+  },
+  /* once axios is mounted, automatically sends get request to pull all hardware */
+  mounted(){
+    this.hardwareData = [];
+    axios.get(apiURL + '/all')
+    /* array to store response data */
+    .then((resp) => {
+      /* takes response from get request and compiles it into array */
+      this.hardwareData = resp.data;
+    });
+  },
+  methods: {
+    /* method to handle form submission*/
+    async submitForm() {       
+        axios
+          /* sends POST request through axios to backend, alerts user of success, then reloads page through router */
+          .post(apiURL, this.hardware)
+          .then(() => {
+            alert("Hardware has been succesfully added.");
+            window.location.reload();
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    },
+    /* method for deleting hardware type */
+    deleteHtype(hardwareid) {
+      if(confirm("Are you sure you want to permanently delete this Hardware Type? This cannot be undone.")){
+      /* axios delete request, uses api URL and attaches id at end of it to specify what id to delete */ 
+        axios.delete(apiURL+"?id=" + hardwareid).then(() => {
+          /* reloads window to show changes */
+          window.location.reload();
+        });
+      }
+    },
+    /* method for routing to edit page */
+    editHTYPE(hardware_id) {
+      /* Activates on click of table property, routes to update page bases on name in index.js, params are the id of the item which is stored in id:  */
+      this.$router.push({ name: "updatehardware", params: {id: hardware_id}})
+    },
+
+  },
 }
 </script>
 
