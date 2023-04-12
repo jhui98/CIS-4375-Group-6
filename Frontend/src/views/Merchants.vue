ce5f19a7ac18babe45c68134f6ae962c24c1efb5
JoshuaASoto <joshua.antonio.soto@gmail.com>
2023-04-11 22:55:31 -0500

    Formatting Changes

--- Frontend/src/views/Merchants.vue
+++ Frontend/src/views/Merchants.vue
@@ -2,7 +2,7 @@
 import axios from "axios";
 // backend endpoint for list of all merchants
 let apiURL = `http://127.0.0.1:5000/api/merchant`;
-//let resellerURL = `http://127.0.0.1:5000/api/reseller/all`;
+let resellerURL = `http://127.0.0.1:5000/api/reseller/all`;
 export default {
   data() {
     return {
@@ -17,19 +17,28 @@ export default {
         merchant_email: "",
         merchant_phone: "",
         reseller_name: "",
+        reseller_id: ""
+
       },
       merchantData: [],
+      resellerData: []
     };
   },
   /* once axios is mounted, automatically sends get request to pull all merchants */
-  mounted() {
+  async mounted() {
     /* array to store response data */
     this.merchantData = [];
-    axios
+    await axios
       .get(apiURL + "/all")
       /* takes response from get request and compiles it into array */
       .then((resp) => {
         this.merchantData = resp.data;
+      }),
+      this.resellerData = [];
+
+      await axios.get(resellerURL)
+      .then((resp) => {
+        this.resellerData = resp.data;
       });
   },
   methods: {
@@ -70,12 +79,193 @@ export default {
 };
 </script>
 
+<!--Styling of the Merchant webpage-->
+<style>
+@import "bootswatch/dist/flatly/bootstrap.min.css";
+
+* {
+  box-sizing: border-box;
+}
+
+.column {
+  float: left;
+  width: 33.33%;
+  height: 100px;
+  border-style: double;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: border-box;
+  clear: both;
+}
+.delete {
+  background-color: #f44336; /* Red */
+  border: none;
+  color: white;
+  padding: 15px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 12px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
+
 <template>
   <main class="home-page">
     <h1>Merchants Operations</h1>
     <br />
-    <h4 text-align="center">Register a New Merchant</h4>
-    <div class="px-20 py-20">
+    <div>
+      <form @submit.prevent="submitForm">
+      <legend>Register New Merchant</legend>
+      <div class="form-group col-sm-6">
+        <div class="row">
+
+          <div class="col-12">
+            <label class="form-label mt-4">
+                  <!-- asterisk to denote required field-->
+                  <span style="color:#ff0000">* </span>
+                  <span class="text">Merchant Name</span></label>
+                <div class="col-sm-9">
+                  <input
+                type="text" class="form-control"
+                v-model="merchant.merchant_name"
+                placeholder="Merchant name"
+              /> 
+            </div>  
+          </div>
+
+        </div>
+        <div class="row">
+
+          <div class="col-6">
+            <label class="form-label mt-4">
+                  <!-- asterisk to denote required field-->
+                  <span style="color:#ff0000">* </span>
+                  <span class="text">Phone Number</span></label>
+                <div class="col-sm-6">
+                  <input
+                type="text" class="form-control"
+                v-model="merchant.merchant_phone"
+                placeholder="(xxx)-xxx-xxxx"
+              /> 
+            </div>  
+          </div>
+
+          <div class="col-6">
+            <label class="form-label mt-4">
+                  <!-- asterisk to denote required field-->
+                  <span style="color:#ff0000">* </span>
+                  <span class="text">Email</span></label>
+                <div class="col-sm-10">
+                  <input
+                type="text" class="form-control"
+                v-model="merchant.merchant_email"
+                placeholder="example@xyz.com"
+              /> 
+            </div>  
+          </div>
+
+        </div>
+        <br>
+        <div class="row">
+          <div class="col-6">
+            <label class="form-label mt-4">
+                  <!-- asterisk to denote required field-->
+                  <span style="color:#ff0000">* </span>
+                  <span class="text">Adress Line 1</span></label>
+                <div class="col-sm-10">
+                  <input
+                type="text" class="form-control"
+                v-model="merchant.merchant_address1"
+                placeholder="Address 1"
+              /> 
+            </div>  
+          </div>
+          <div class="col-6">
+            <label class="form-label mt-4">
+                  <!-- asterisk to denote required field-->
+                  <span style="color:#ff0000"> </span>
+                  <span class="text">Adress Line 2</span></label>
+                <div class="col-sm-10">
+                  <input
+                type="text" class="form-control"
+                v-model="merchant.merchant_address2"
+                placeholder="Address 2"
+              /> 
+            </div>  
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-4">
+            <label class="form-label mt-4">
+                  <!-- asterisk to denote required field-->
+                  <span style="color:#ff0000"> </span>
+                  <span class="text">City</span></label>
+                <div class="col-sm-10">
+                  <input
+                type="text" class="form-control"
+                v-model="merchant.merchant_city"
+                placeholder="Houston"
+              /> 
+            </div>  
+          </div>
+          <div class="col-4">
+            <label class="form-label mt-4">
+                  <!-- asterisk to denote required field-->
+                  <span style="color:#ff0000"> </span>
+                  <span class="text">State</span></label>
+                <div class="col-sm-10">
+                  <input
+                type="text" class="form-control"
+                v-model="merchant.merchant_state"
+                placeholder="Texas"
+              /> 
+            </div>  
+          </div>
+          <div class="col-4">
+            <label class="form-label mt-4">
+                  <!-- asterisk to denote required field-->
+                  <span style="color:#ff0000"> </span>
+                  <span class="text">Zip Code</span></label>
+                <div class="col-sm-10">
+                  <input
+                type="text" class="form-control"
+                v-model="merchant.merchant_zip"
+                placeholder="XXXXX"
+              /> 
+            </div>  
+          </div>
+        </div>
+          <br>
+          <div class="col-4">
+            <label class="form-label mt-4">
+                  <!-- asterisk to denote required field-->
+                  <span style="color:#ff0000"> </span>
+                  <span class="text">Reseller</span></label>
+                <div class="col-sm-10">
+                  <select class="form-select" v-model="merchant.reseller_id">
+                    <option v-for="item in resellerData" :key="item.reseller_id" :value="item.reseller_id">
+                      {{ item.reseller_name }}
+                    </option>
+                  </select> 
+            </div>  
+          </div> 
+          <!--Submit Button-->
+          <div>
+            <br>
+            <button type="submit" class="btn btn-info">Add Merchant</button>
+          </div>
+          
+    
+        
+        </div>
+      </form>
+
+
       <!-- @submit.prevent stops the submit event from reloading the page-->
       <form @submit.prevent="submitForm">
         <!-- grid container -->
@@ -253,46 +443,5 @@ export default {
     </div>
   </main>
 </template>
-<style>
-* {
-  box-sizing: border-box;
-}
 
-.column {
-  float: left;
-  width: 33.33%;
-  height: 100px;
-  border-style: double;
-}
 
-/* Clear floats after the columns */
-.row:after {
-  content: "";
-  display: border-box;
-  clear: both;
-}
-.add {
-  background-color: #008cba; /* Blue */
-  border: none;
-  color: white;
-  padding: 15px 25px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 12px;
-  margin: 4px 2px;
-  cursor: pointer;
-}
-.delete {
-  background-color: #f44336; /* Red */
-  border: none;
-  color: white;
-  padding: 15px 25px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 12px;
-  margin: 4px 2px;
-  cursor: pointer;
-}
-</style>
