695a02d336876b462f1efcbf3e9c10ac1e0ef8e9
JoshuaASoto <joshua.antonio.soto@gmail.com>
2023-04-16 12:34:35 -0500

    Added Open Order Table

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -3,18 +3,18 @@ import axios from "axios"
 export default{
     data() {
         return {
-            isoData:[],
+            openData:[],
         };
     },
 
     methods:{
         //Get Function for ISO table, used to fill in table
         getISO() {
-            this.isoData = [];
-            const path = `http://localhost:5000/api/iso/all`;
+            this.openData = [];
+            const path = `http://localhost:5000/api/orders/open`;
             axios.get(path)
             .then((resp) => {
-                this.isoData = resp.data;
+                this.openData = resp.data;
             })
             .catch((err) => {
                 console.error(err);
@@ -28,83 +28,102 @@ export default{
 
 </script>
 
+<!--Styling of the Home webpage-->
+<style>
+@import "bootswatch/dist/flatly/bootstrap.min.css";
+
+* {
+  box-sizing: border-box;
+}
+
+.column {
+  float: left;
+  width: 33.33%;
+  height: 100px;
+  border-style: double;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: border-box;
+  clear: both;
+}
+.delete {
+  background-color: #f44336; /* Red */
+  border: none;
+  color: white;
+  padding: 15px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 12px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
+
 <template>
     <main class="home-page">
         <h1>Home</h1>
-        <p>This is the home page</p>
-
-
-        <div>Selected: {{ selected }}</div>
-
-        <select v-model="selected">
-            <option v-for="item in isoData" :value="item.ISO_ID"> 
-            {{ item.ISO_COMPANY }}
-            </option>
-        </select>
-
-        <!-- <div class="jumbotron vertical center">
-            <div class="container">
-                
-                <div class="row">
-                    <div class="col-sm-12">
-                        <p> Header</p>
-                        <hr><br>
-
-                        <button type="button" class="btn btn-success
-                        btn-sm" v-b-modal.iso-modal>Add ISO</button>
-                        <br><br>
-                        <table class="table table-hover">
-                          
-                            <thead>
-                                <tr>
-                                    
-                                    <th scope="col">ID</th>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Action</th>
-
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr v-for="item, ISO_ID in isoData" :key="item.ISO_ID">
-                                    <td> {{item.ISO_ID}} </td>
-                                    <td> {{ item.ISO_COMPANY }} </td>
-                                    <td> 
-                                        <div class="btn-group" role="group">
-                                            <button type="button" class="btn btn-info
-                                            btn-sm">Update</button>
-                                            <button type="button" class="btn btn-danger
-                                            btn-sm">Delete</button>
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-
-                        </table>
+        <br>
+        <h4>All Open Orders are listed below</h4>
+    <div class="jumbotron vertical center">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <hr />
+            <br />
+            <table class="table table-hover">
+              <!-- Table Head-->
+              <thead>
+                <tr>
+                  <!--Table Head cells-->
+                  <th scope="col">Order #</th>
+                  <th scope="col">Order Date</th>
+                  <th scope="col">Ship Date</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in openData" :key="item.ORDER_ID">
+                  <!--Do no show this to user-->
+                  <!-- <td>{{ item.order_id }}</td> -->
+                  <td>{{ item.ORDER_NUM }}</td>
+                  <td>{{ item.ORDER_DATE }}</td>
+                  <!-- Searches through all pulled merchants and finds record that matches the item's merchant_id, then returns its name - Thanks Zach :) -->
+                  <td>{{ item.SHIP_DATE }}</td>
+                  <td>
+                    <div class="btn-group" role="group">
+                      <!--Update Button-->
+                      <button
+                        type="button"
+                        class="btn btn-info btn-sm"
+                     
+                      >
+                        Update
+                      </button>
+                      <!--Delete Button-->
+                      <button
+                        type="button"
+                        class="btn btn-danger btn-sm"
+                   
+                      >
+                        Delete
+                      </button>
                     </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+        
 
-                </div>
-            </div>
 
-        </div> -->
     </main>
 
-    <!-- <b-modal ref="addISOModal" id="iso-modal" title="Add an ISO" >
-        <b-form @submit ="onSubmit" @reset="onReset" class="w-100">
-            <b-form-group id="form-title-group" label="Title:" label-for="form-title-input">
-                <b-form-input id="form-title-input"
-                type="text"
-                v-model="addISOForm.isoName"
-                required
-                placeholder = "Enter ISO Name">
-
-                </b-form-input>
-            </b-form-group>
-
-       
-            <button type="submit" varient="primary">Submit</button>
-            <button type="reset" varient="primary">Reset</button>
-        </b-form>
-
-    </b-modal> -->
 
 </template>
